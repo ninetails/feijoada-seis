8143e8c095ec7638e9c355d6cbe253e39eaefa58
Carlos Kazuo Mochizuki Ishizaka <bld_cishizaka@uolinc.com>
2018-06-13 23:02:50 -0300

    Done part II

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const hapi = require('hapi')
 const mongoose = require('mongoose')
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi')
+const Inert = require('inert')
+const Vision = require('vision')
+const HapiSwagger = require('hapi-swagger')
+const Pack = require('./package.json')
+
+const schema = require('./graphql/schema')
 
 const Painting = require('./models/Painting')
 
@@ -13,6 +20,46 @@ const server = hapi.server({
 })
 
 const init = async () => {
+  await server.register({
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql'
+      },
+      route: {
+        cors: true
+      }
+    }
+  })
+
+  await server.register({
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: {
+        schema
+      },
+      route: {
+        cors: true
+      }
+    }
+  })
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Paintings API Documentation',
+          version: Pack.version
+        }
+      }
+    }
+  ])
+
   server.route([
     {
       method: 'GET',
@@ -22,15 +69,23 @@ const init = async () => {
     {
       method: 'GET',
       path: '/api/v1/paintings',
+      config: {
+        description: 'Get all the paintings',
+        tags: ['api', 'v1', 'painting']
+      },
       handler: () => Painting.find()
     },
     {
       method: 'POST',
       path: '/api/v1/paintings',
+      config: {
+        description: 'Adds a painting',
+        tags: ['api', 'v1', 'painting']
+      },
       handler: req => {
-        const { name, url, techniques } = req.payload
+        const { name, url, technique } = req.payload
         const painting = new Painting({
-          name, url, techniques
+          name, url, technique
         })
 
         return painting.save()
--- models/Painting.js
+++ models/Painting.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 const PaintingSchema = new Schema({
   name: String,
   url: String,
-  techniques: [String]
+  technique: String
 })
 
 module.exports = mongoose.model('Painting', PaintingSchema)
